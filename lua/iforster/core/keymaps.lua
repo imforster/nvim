00ed572be3f1c1c8fbdaff8a03479111cefbbca3
Ian Forster <ian.forster@gmail.com>
2024-03-26 22:35:01 -0700

    add switch mapping.

--- lua/iforster/core/keymaps.lua
+++ lua/iforster/core/keymaps.lua
@@ -32,6 +32,7 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window max
 
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>s", ":NvimTreeFocus<CR>") -- toggle file explorer
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
