f7dfca8d3fbeb88f235e769a4215e8e9d2a106ca
Ian Forster <ian.forster@gmail.com>
2024-04-21 20:27:38 -0700

    small changes

--- lua/iforster/plugins-setup.lua
+++ lua/iforster/plugins-setup.lua
@@ -120,4 +120,18 @@ return packer.startup(function(use)
   if packer_bootstrap then
     require("packer").sync()
   end
+  -- Lua
+  -- whick key
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
 end)
