7b441c738de72effe39eaaefc28f0b673c243eca
Ian Forster <ian.forster@gmail.com>
2024-05-19 11:33:47 -0700

    Updated with instruction from lazy to properly load plugins.

---
+++ lua/iforster/core/init.lua
@@ -0,0 +1,4 @@
+require("iforster.core.fonts")
+require("iforster.core.options")
+require("iforster.core.keymaps")
+require("iforster.core.colorscheme")
--- lua/iforster/plugins-setup.lua
+++ lua/iforster/plugins-setup.lua
@@ -1,15 +1,15 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+--local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--  vim.fn.system({
+--    "git",
+--    "clone",
+--    "--filter=blob:none",
+--    "https://github.com/folke/lazy.nvim.git",
+--    "--branch=stable", -- latest stable release
+--    lazypath,
+--  })
+--end
+--vim.opt.rtp:prepend(lazypath)
 
 -- Example using a list of specs with the default options
 vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappings are correct
@@ -201,6 +201,31 @@ require("lazy").setup({
   "L3MON4D3/LuaSnip",-- snippet engine
   "saadparwaiz1/cmp_luasnip",-- for autocompletion
   "rafamadriz/friendly-snippets", -- useful snippets
+
+
+  -- managing & installing lsp servers, linters & formatters
+  "williamboman/mason.nvim",-- in charge of managing lsp servers, linters & formatters
+  "williamboman/mason-lspconfig.nvim",-- bridges gap b/w mason & lspconfig
+  "mfussenegger/nvim-dap",
+  "mfussenegger/nvim-jdtls",
+
+  -- configuring lsp servers
+  "neovim/nvim-lspconfig", -- easily configure language servers
+  "hrsh7th/cmp-nvim-lsp", -- for autocompletion
+  {
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    requires = {
+      { "nvim-tree/nvim-web-devicons" },
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+  }, -- enhanced lsp uis
+  "jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename file & update imports)
+  "onsails/lspkind.nvim",-- vs-code like icons for autocompletion
+
+  -- auto closing
+  "windwp/nvim-autopairs", -- autoclose parens, brackets, quotes, etc...
+  { "windwp/nvim-ts-autotag", after = "nvim-treesitter" }, -- autoclose tags
 
   -- formatting & linting
   "jose-elias-alvarez/null-ls.nvim", -- configure formatters & linters
