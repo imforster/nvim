f73cb85471132107b7d8040302b30d211f203207
Ian Forster <ian.forster@gmail.com>
2024-06-30 23:30:41 -0700

    Comment out config for telescope and typescript added as language binding.

--- lua/iforster/plugins/dashboard.lua
+++ lua/iforster/plugins/dashboard.lua
@@ -30,7 +30,7 @@ return {
           { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
           { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
           { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
-          { action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+          --{ action = [[lua LazyVim.telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
           { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
           { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
           { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
