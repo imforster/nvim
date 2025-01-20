187886575e5f5fcf114c419d769119dd5f0c9060
Ian Forster <ian.forster@gmail.com>
2025-01-19 20:06:17 -0800

    Updates

--- lua/iforster/plugins/telescope.lua
+++ lua/iforster/plugins/telescope.lua
@@ -30,13 +30,13 @@ return {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-						["<C-t>"] = trouble_telescope.smart,
+						["<C-t>"] = trouble_telescope.open,
 					},
 				},
 			},
 		})
 
-		telescope.load_extension("fzf")
+		--		telescope.load_extension("fzf")
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
